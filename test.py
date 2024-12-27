0e6efdafbfd4925c0ed989e90fbe2d1ab998a7e5
Paul Höljes <paul.hapunkt@hotmail.de>
2024-12-28 00:29:55 +0100

    interaction matrix, energy calculation, acceptance and rejection of folds

--- test.py
+++ test.py
@@ -1,27 +1,26 @@
-
+from interaction import Interaction
 import network
 import numpy as np
 from numba import jit
 
 @jit() # doesn't work without jit
 def set_seed():
-    np.random.seed(123456789)
+    np.random.seed(1)
 
-set_seed()
-p = network.Protein()
+#set_seed()
+J = Interaction().matrix
+p = network.Protein(J)
 
 network.plot_random_walk(p.chain)
 print(p.chain)
-print(np.flip(p.grid.T, axis=0))
+#print(np.flip(p.grid.T, axis=0))
 
 def fold(x, y):
     print(p.fold_step_at(x, y, False))
     network.plot_random_walk(p.chain)
 
-fold(3, 5) # True
-fold(5, 5) # False
-fold(5, 3) # True
-fold(3, 3) # True
-
-print(p.chain)
-print(np.flip(p.grid.T, axis=0)) # print correct representation of grid
+print('START LOOP')
+for i in range(10):
+    p.random_fold_step(0.1)
+    network.plot_random_walk(p.chain)
+    print(p.calc_energy(p.chain))

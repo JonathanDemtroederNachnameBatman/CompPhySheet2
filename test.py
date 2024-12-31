b570ab1ac5d6e6f76555be9158664ad3467d627a
Paul Höljes <paul.hapunkt@hotmail.de>
2024-12-31 01:00:58 +0100

    ex4

--- test.py
+++ test.py
@@ -1,24 +1,30 @@
+from matplotlib import pyplot as plt
+
 import network
 import numpy as np
-from numba import jit
+from numba import jit, double, int8, int64
 
-@jit() # doesn't work without jit
-def set_seed():
-    np.random.seed(1)
 
-#set_seed()
-p = network.create_protein(interaction_type='normal')
+@jit(double[:](int8, int64))
+def monte_carlo_const_temperature(temperature, number_of_steps):
+    simulation = np.zeros(number_of_steps, dtype=double)
 
-network.plot_random_walk(p.chain)
-print(p.chain)
-#print(np.flip(p.grid.T, axis=0))
+    p = network.create_protein(interaction_type='normal')
+    simulation[0] = p.energy
+    for step in range(1, number_of_steps):
+        p.random_fold_step(temperature)
+        simulation[step] = p.energy
 
-def fold(x, y):
-    print(p.fold_step_at(x, y, False))
-    network.plot_random_walk(p.chain)
+    return simulation
 
-print('START LOOP')
-for i in range(10):
-    p.random_fold_step(0.1)
-    network.plot_random_walk(p.chain)
-    print(p.calc_energy(p.chain))
+steps = 1e6
+temperature = 1
+
+arr = monte_carlo_const_temperature(temperature, steps)
+
+fig, ax = plt.subplots()
+
+ax.set_xlabel('Monte-Carlo-Schritte')
+ax.set_ylabel('Energie in a.u.')
+ax.plot(np.arange(steps), arr)
+plt.savefig('test')
